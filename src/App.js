dd5d31157a6f35188481fee2b72d60b45e7b8376
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-02-26 18:08:17 -0600

    added first props and children

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import { CardList } from "./components/card-list/CardList";
 
 class App extends Component {
   constructor() {
@@ -17,9 +18,11 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList name="Can">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
