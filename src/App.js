afb5bed25c941851be5a5ab3fe98ece3d0644439
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-02-26 19:38:35 -0600

    monsters added and modified with css card

--- src/App.js
+++ src/App.js
@@ -18,11 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <CardList name="Can">
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
