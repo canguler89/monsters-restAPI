ddb2acfdb385a1234279da07cfc305255f7afdc7
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-03-02 16:04:51 -0600

    created dynamic search box

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -16,9 +17,19 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="app">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type="search"
+          placeholder="Search monsters..."
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
