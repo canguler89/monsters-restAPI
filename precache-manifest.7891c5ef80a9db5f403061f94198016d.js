5a9906493ecb130d32c79a6dc830dc05d4576e5c
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-03-02 18:23:54 -0600

    Updates

---
+++ precache-manifest.7891c5ef80a9db5f403061f94198016d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc17156face1febedf2b2dcc354305f",
+    "url": "/monsters-restAPI/index.html"
+  },
+  {
+    "revision": "c62573912164bffe9574",
+    "url": "/monsters-restAPI/static/css/main.1d92bf2c.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters-restAPI/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-restAPI/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62573912164bffe9574",
+    "url": "/monsters-restAPI/static/js/main.16c6398d.chunk.js"
+  },
+  {
+    "revision": "251914d92e2745de997c",
+    "url": "/monsters-restAPI/static/js/runtime-main.72e16cc8.js"
+  }
+]);
